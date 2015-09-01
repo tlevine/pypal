31430fc5e02e75f58e81bbc561d6c8e35d2d5db7
Thomas Levine <_@thomaslevine.com>
2015-09-01 09:09:00 -0400

    mostly copy parsers from the dadawarehouse

---
+++ pal/parsers.py
@@ -0,0 +1,32 @@
+import datetime
+
+def entry(line):
+    'Read a pal calendar entry'
+    datespec, _, description = line.partition(' ')
+    for date in dates(datespec):
+        yield date, description
+
+def read_date(datestring):
+    try:
+        date = datetime.datetime.strptime(datestring, '%Y%m%d')
+    except ValueError:
+        return None
+    else:
+        return date.date()
+
+def dates(datespec, today = datetime.date.today()):
+    single_date = read_date(datespec)
+    if single_date != None:
+        yield single_date
+    elif datespec.count(':') == 2:
+        subset, _start, _end = datespec.split(':')
+        if subset == 'DAILY':
+            current = read_date(_start)
+            end = read_date(_end)
+            while current <= end:
+                yield current
+                current += datetime.timedelta(days = 1)
+    elif datespec == 'TODO':
+        yield today
+    else:
+        logger.warn('Unsupported date specification: "%s"' % datespec)
