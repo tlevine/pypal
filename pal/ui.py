fd79caf707781654e8ccbdac09d1da7bd3c868f1
Thomas Levine <_@thomaslevine.com>
2015-09-04 07:53:55 -0400

    date range

--- pal/ui.py
+++ pal/ui.py
@@ -1,5 +1,21 @@
 import argparse
+import datetime
+
+from date_range import date_range
 
 from .parser import file
 
-argparse.ArgumentParser(prog = 'pypal')
+p = argparse.ArgumentParser(prog = 'pypal')
+p.add_argument('-r', help = 'Date range', default = '0')
+
+def main():
+    args = p.parse_args()
+    try:
+        forward = int(args.r)
+    except ValueError:
+        forward, back = map(int, args.r.split('-'))
+    else:
+        back = 0
+
+    t = datetime.date.today()
+    dates = date_range(t - datetime.timedelta(back), t + datetime.timedelta(forward + 1))
